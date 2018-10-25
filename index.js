d9311ebbd7b8f320ace388a3f51ba6edc0bbaa00
Orlando Leite <orlleite@gmail.com>
2018-10-25 18:53:07 -0300

    Fixed function already exists

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
-Object.defineProperty(Array.prototype, 'diff',
+if(!Array.prototype.diff)
 {
-    enumerable: false,
-    value: function (array)
+    Object.defineProperty(Array.prototype, 'diff',
     {
-        return this.filter(function (i) { return array.indexOf(i) < 0; });
-    }
-});
+        enumerable: false,
+        value: function (array)
+        {
+            return this.filter(function (i) { return array.indexOf(i) < 0; });
+        }
+    });
+}
